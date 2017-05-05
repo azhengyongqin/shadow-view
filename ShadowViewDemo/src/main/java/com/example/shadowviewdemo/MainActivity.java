fe837bd77d1805e63410e1ab9e3a9e56cd5ef382
每一段路丶都是一种领悟 <1023007219@qq.com>
2017-05-05 21:56:47 +0800

    增加通过java代码set方式设置自定义属性

--- ShadowViewDemo/src/main/java/com/example/shadowviewdemo/MainActivity.java
+++ ShadowViewDemo/src/main/java/com/example/shadowviewdemo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.shadowviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.shadowviewlibrary.CustomShadowView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CustomShadowView shadowView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        shadowView = (CustomShadowView) findViewById(R.id.shadow_view);
+        textView = (TextView) findViewById(R.id.tv_test);
+        textView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
     }
 }
--- ShadowViewLibrary/src/main/java/com/example/shadowviewlibrary/CustomShadowView.java
+++ ShadowViewLibrary/src/main/java/com/example/shadowviewlibrary/CustomShadowView.java
@@ -8,8 +8,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Build;
+import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -20,26 +20,25 @@ import android.view.View;
  * 作 者: 郑亮
  * Q  Q : 1023007219
  */
-@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class CustomShadowView extends View {
     private static final String TAG = "CustomShadowView";
     private Paint mPaint;
     /**
      * 模糊半径
      */
-    private static final float fuzzyRadius = 25f;
+    private float shadow_fuzzy_radius = 25f;
     /**
      * 阴影颜色
      */
-    private int shadow_color;
+    private int shadow_color = Color.GRAY;
     /**
      * 阴影高度
      */
-    private float shadow_height;
+    private float shadow_height = 25f;
     /**
      * 阴影位置
      */
-    private int shadow_position;
+    private int shadow_position = SHADOW_DOWN;
     /**
      * 位置
      */
@@ -71,7 +70,6 @@ public class CustomShadowView extends View {
         super(context, attrs, defStyleAttr);
         init(attrs);
     }
-
     private void init(AttributeSet attrs) {
         TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.CustomShadowView);
         /*初始化自定义属性*/
@@ -79,7 +77,9 @@ public class CustomShadowView extends View {
         mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
         this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        this.setElevation(-1);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.setElevation(-1);
+        }
     }
 
     /**
@@ -88,9 +88,10 @@ public class CustomShadowView extends View {
      */
     private void initAttrs(TypedArray typedArray) {
         if (typedArray != null) {
-            shadow_color = typedArray.getColor(R.styleable.CustomShadowView_shadow_color, 0x5549c4fa);
+            shadow_color = typedArray.getColor(R.styleable.CustomShadowView_shadow_color, Color.GRAY);
             shadow_height = typedArray.getDimension(R.styleable.CustomShadowView_shadow_height, 25);
             shadow_position = typedArray.getInt(R.styleable.CustomShadowView_shadow_position, 2);
+            shadow_fuzzy_radius = typedArray.getFloat(R.styleable.CustomShadowView_shadow_fuzzy_radius,25f);
             view_id = typedArray.getResourceId(R.styleable.CustomShadowView_view_id, 0);
             typedArray.recycle();
         }
@@ -126,11 +127,11 @@ public class CustomShadowView extends View {
         super.onDraw(canvas);
         //设置画笔的阴影，param1：模糊半径；param2：x轴大小：param3：y轴大小；param4：阴影颜色
         if (shadow_position == SHADOW_UP) {//阴影在上面
-            mPaint.setShadowLayer(fuzzyRadius, 0, -shadow_height, shadow_color);
-            rect = new RectF(left, top, right, bottom-((bottom-top)/2));
+            mPaint.setShadowLayer(shadow_fuzzy_radius, 0, -shadow_height, shadow_color);
+            rect = new RectF(left, top, right, bottom-((bottom-top))/4);
         } else if (shadow_position == SHADOW_DOWN) {//阴影在下面
-            mPaint.setShadowLayer(fuzzyRadius, 0, shadow_height, shadow_color);
-            rect = new RectF(left, top+((bottom-top)/2), right, bottom);
+            mPaint.setShadowLayer(shadow_fuzzy_radius,0, shadow_height, shadow_color);
+            rect = new RectF(left, top+((bottom-top)/4), right, bottom);
         }
         canvas.drawRect(rect, mPaint);
     }
@@ -145,14 +146,49 @@ public class CustomShadowView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-
         if (this.view != null) {
-
             this.left = this.view.getLeft();
             this.top = this.view.getTop();
             this.right = this.view.getRight();
             this.bottom = this.view.getBottom();
         }
+        Log.e(TAG, "onMeasure: 宽:"+this.right+"高:"+this.bottom );
+        setMeasuredDimension(this.right, this.bottom);
+    }
 
+    public float getShadow_fuzzy_radius() {
+        return shadow_fuzzy_radius;
+    }
+
+    public void setShadow_fuzzy_radius(float shadow_fuzzy_radius){
+        this.shadow_fuzzy_radius = shadow_fuzzy_radius;
+        invalidate();
+    }
+
+    public int getShadow_color() {
+        return shadow_color;
+    }
+
+    public void setShadow_color(@ColorInt int shadow_color) {
+        this.shadow_color = shadow_color;
+        invalidate();
+    }
+
+    public float getShadow_height() {
+        return shadow_height;
+    }
+
+    public void setShadow_height(float shadow_height) {
+        this.shadow_height = shadow_height;
+        invalidate();
+    }
+
+    public int getShadow_position() {
+        return shadow_position;
+    }
+
+    public void setShadow_position(int shadow_position) {
+        this.shadow_position = shadow_position;
+        invalidate();
     }
 }
